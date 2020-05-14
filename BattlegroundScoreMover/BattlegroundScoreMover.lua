e6e799e124dacb8d0a8fa4ec3cd1edf337968f2f
voodoom-fr <voodoom.dev@gmail.com>
2020-05-14 21:57:01 +0200

    Adding LibDebugLogger as dependency
    Signed-off-by: voodoom-fr <voodoom.dev@gmail.com>

--- BattlegroundScoreMover/BattlegroundScoreMover.lua
+++ BattlegroundScoreMover/BattlegroundScoreMover.lua
@@ -2,13 +2,15 @@ BSM = {}
 
 BSM.name = "BattlegroundScoreMover"
 
+local logger = LibDebugLogger("BattlegroundScoreMover")
+
 function BSM:Initialize()
     self.inCombat = IsUnitInCombat("player")
     EVENT_MANAGER:RegisterForEvent(self.name, EVENT_PLAYER_COMBAT_STATE, self.OnPlayerCombatState)
 end
 
 function BSM.OnAddonLoaded(event, addonName)
-    d("BSM loading test")
+    logger:Debug("BSM loading debug message")
     if addonName == BSM.name then
         BSM:Initialize()
         EVENT_MANAGER:UnregisterForEvent(BSM.name, EVENT_ADDON_ON_LOADED)
@@ -16,13 +18,16 @@ function BSM.OnAddonLoaded(event, addonName)
 end
 
 function BSM.OnPlayerCombatState(event, inCombat)
+    logger:Debug("Combat: " ..tostring(inCombat))
     d("Combat: " ..tostring(inCombat))
     if inCombat ~= BSM.inCombat then
         BSM.inCombat = inCombat
 
         if inCombat then
+            logger:Debug("Entering combat.")
             d("Entering combat.")
         else
+            logger:Debug("Exiting combat.")
             d("Exiting combat.")
         end
 
